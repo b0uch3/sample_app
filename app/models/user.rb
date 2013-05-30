91e42d2ae0da1c74f17dfed430684729e434ffca
Mike Bouchard <bouche@gmail.com>
2013-05-29 21:56:35 -0400

    did more signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
- before_save { self.email = email.downcase }
+  before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
